d41c69ac908d0a6f522582e33ffe36914147d6d4
Syed-Zahaab-Hussain <144938300+Syed-Zahaab-Hussain@users.noreply.github.com>
2024-11-06 18:20:38 +0500

    fix the cors

--- api/index.js
+++ api/index.js
@@ -32,12 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:4173",
-      "http://localhost:3000",
-    ],
+    origin: process.env.VITE_BASE_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
